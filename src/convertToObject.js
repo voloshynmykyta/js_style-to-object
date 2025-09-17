884a277c1b718b53de81ba25709f3168ef255366
Nikita Voloshyn <nikita.voloshyn@qpard.com>
2025-09-17 15:24:18 +0300

    implement css to object convertor

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (!sourceString.length) {
+    return {};
+  }
+
+  const properties = sourceString
+    .split(';')
+    .map((el) => el.trim())
+    .filter((el) => el !== '');
+
+  return properties.reduce((acc, property) => {
+    const colonIndex = property.indexOf(':');
+
+    const key = property.slice(0, colonIndex).trim();
+    const value = property.slice(colonIndex + 1).trim();
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
 }
 
 module.exports = convertToObject;
